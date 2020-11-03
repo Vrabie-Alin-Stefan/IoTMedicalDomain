56e7df00b2ce8b25ca90eb23455025748e25a192
Vrabie-Alin-Stefan <35737890+Vrabie-Alin-Stefan@users.noreply.github.com>
2020-11-03 23:58:43 +0200

    Create Doctor.java

---
+++ DisignsPatternsDemo_Alin/Prototype/Doctor.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+public class Doctor extends User {
+
+    private String userType;
+
+    public Doctor()
+    {
+        this.userType = "Doctor";
+    }
+    @Override
+    void addUser() {
+
+    }
+}
